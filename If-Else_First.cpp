1611f7e2ba9f26e3d026b6531ca6bbd957f37ba5
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-03-22 21:50:39 +0530

    First If-Else Program

---
+++ If-Else_First.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    if(a>b)
+    {
+        cout<<"a: "<<a<<" is greater";
+    }
+    else if(b>a)
+    {
+        cout<<"b: "<<b<<" is greater";
+    }
+    else
+    {
+        cout<<"Both are equal.";
+    }
+
+    return 0;
+}
