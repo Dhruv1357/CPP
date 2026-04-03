7dafbff21967c0d1ae0af8d277859508ce86e411
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-03 21:59:55 +0530

    Time and Space Complexity

---
+++ Time_Space-Complexity-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    int a = 0, i = N;
+    while (i > 0)
+    {
+        a += i;
+        i /= 2;
+    }
+}
+// Time Complexity - O(log(N))
+// Space Cpmplexity - O(1)
