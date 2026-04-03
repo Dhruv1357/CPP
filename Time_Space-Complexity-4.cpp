fd5af678085a519e63f31c631bb61d58781ae78f
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-03 22:01:54 +0530

    Time and Space Complexity

---
+++ Time_Space-Complexity-4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int count = 0,N;
+        for (int i = N; i > 0; i /= 2) {
+            for (int j = 0; j < i; j++) {
+                count += 1;
+            }
+        }
+}
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
