7374e8fafa3e75f424e509232df6e03fa514a469
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-05 22:10:43 +0530

    improve design and styling on line 19

--- Is_number_prime.cpp
+++ Is_number_prime.cpp
@@ -16,8 +16,7 @@ int main()
 
     if (n > 1)
     {
-        for (int i = 2; i * i <= n; i++)
-        {
+        for (int i = 2; i * i <= n; i++){
             if (n % i == 0)
             {
                 cout << "Not prime";
