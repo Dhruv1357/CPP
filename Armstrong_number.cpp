77624c0385aba567d493321778b2fdc543a310c1
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-11 23:08:18 +0530

    Finding if the number is an Armstrong Number or not

---
+++ Armstrong_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool armStrongNumber(int n)
+{
+    // code here
+    int sum = 0, num = n, x;
+
+    for (int i = 0; i < 3; i++)
+    {
+        x = num % 10;
+        sum = sum + (x * x * x);
+        num /= 10;
+    }
+    if (sum == n)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << armStrongNumber(n);
+}
