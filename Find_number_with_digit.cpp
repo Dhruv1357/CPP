68d12c41791f79e9fc3e1cc15556c577a3a94eba
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-11 23:15:09 +0530

    Finding the number which contains the digit

---
+++ Find_number_with_digit.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solve(int n, int d) 
+{
+    vector<int> arr;
+    int num,x;
+    
+    for(int i=0;i<=n;i++)
+    {
+        num = i;
+        do
+        {
+            x=num%10;
+            if(x == d)
+            {
+                arr.push_back(i);
+                break;
+            }
+            num/=10;
+        }while(num!=0);
+    }
+    
+    if(arr.size() == 0)
+    arr.push_back(-1);
+    
+    return arr;
+}
+
+int main()
+{
+    int n,d;
+    cin >> n >> d;
+
+    solve(n,d);
+}
