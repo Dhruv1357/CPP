28cfa86d5eed6264fd2486806ab629a56a744934
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-13 23:00:53 +0530

    Pronic Number

---
+++ Pronic_Number.cpp
@@ -0,0 +1,37 @@
+/*
+Problem - Pronic Number
+Platform - GeeksForGeeks
+Summary - Finding if the number is pronic or not.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> pronicNumbers(int N) 
+    {
+        vector<int> v;
+        
+        for(int i=0;i<=N;i++)
+        {
+            if(i*(i+1) <= N)
+            {
+                v.push_back(i*(i+1));
+            }
+            
+            if(i*(i+1) > N)
+                break;
+        }
+        return v;
+    }
+    int main()
+    {
+        int num;
+        cin >> num;
+        vector<int> v = pronicNumbers(num);
+
+        for(int y : v)
+        {
+            cout << y << " ";
+        }
+    }
