4c16100e71f19f0bd2430672064e5ab847fa6c48
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-15 21:52:27 +0530

    Factorial Number

---
+++ Factorial_number.cpp
@@ -0,0 +1,28 @@
+/*
+Problem - Factorial Number
+Platform - GeeksForGeeks
+Summary - Finding if the given number is a factorial or not.
+*/
+
+#include <iostream>
+using namespace std;
+
+int isFactorial(int N) {
+
+        int product = 1;
+        
+        for(int i=1;product<=N;i++)
+        {
+            product*=i;
+            if(product == N)
+                return 1;
+        }
+        return 0;
+    }
+
+    int main()
+    {
+        int num;
+        cin >> num;
+        cout << isFactorial(num);
+    }
