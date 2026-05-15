a74e92a51d620cb0422a9850c778c8c22e3388fd
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-15 21:52:46 +0530

    Factorial Number

--- Factorial_number.cpp
+++ Factorial_number.cpp
@@ -7,23 +7,23 @@ Summary - Finding if the given number is a factorial or not.
 #include <iostream>
 using namespace std;
 
-int isFactorial(int N) 
+int isFactorial(int N)
 {
 
-        int product = 1;
-        
-        for(int i=1;product<=N;i++)
-        {
-            product*=i;
-            if(product == N)
-                return 1;
-        }
-        return 0;
-    }
+    int product = 1;
 
-    int main()
+    for (int i = 1; product <= N; i++)
     {
-        int num;
-        cin >> num;
-        cout << isFactorial(num);
+        product *= i;
+        if (product == N)
+            return 1;
     }
+    return 0;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << isFactorial(num);
+}
