8cd8393f469a4e0f5cbd2f146b37d3692b838a46
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-16 22:48:43 +0530

    Is the number a prime number

---
+++ Prime_Number.cpp
@@ -0,0 +1,34 @@
+/*
+Problem - Prime Number
+Platform - GeeksForGeeks
+Summary - Finding if the given number is a prime number or not
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+
+    if (n > 1)
+    {
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+    }
+    else if (n == 1)
+        return false;
+
+    return true;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << isPrime(num);
+}
