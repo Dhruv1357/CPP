58027847dcd5ae26a7bf8ddc03cf20b24bd78a6c
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-16 22:45:29 +0530

    LCM and GCD of two numbers

---
+++ LCM_GCD.cpp
@@ -0,0 +1,56 @@
+/*
+Problem - LCM and GCD
+Platform - GeeksForGeeks
+Summary - Finding the LCM and GCD of two given numbers
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> lcmAndGcd(int a, int b) {
+        // code here
+        int small,large,gcd = 0,lcm;
+        vector<int> v;
+        if(a>b)
+        {
+            large = a;
+            small = b;
+        }
+        else
+        {
+            large = b;
+            small = a;
+        }
+        
+        //lcm check
+        for(int i = 1;;i++)
+        {
+            if((large * i)%small == 0)
+            {
+                v.push_back(large*i);
+                break;
+            }
+        }
+        
+        //gcd check
+        for(int i=1;i<=small;i++)
+        {
+            if((a%i) ==0 && (b%i) == 0)
+            {
+                gcd = i;
+            }
+        }
+        v.push_back(gcd);
+        
+        return v;
+    }
+
+    int main()
+    {
+        int num1,num2;
+        cin >> num1 >> num2;
+        vector<int>v = lcmAndGcd(num1,num2);
+
+        cout << v[0] << " " << v[1];
+    }
