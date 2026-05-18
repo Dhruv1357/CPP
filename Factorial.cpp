43eaf8ebef2ad06e4f2dbdc498bcf01de2c3d1d4
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-18 23:09:12 +0530

    Finding the factorial

---
+++ Factorial.cpp
@@ -0,0 +1,28 @@
+/*
+Problem - Factorial
+Platform - GeeksForGeeks
+Summary - Finding the factorial of the given number n
+*/
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+
+    int factorial = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+
+    return factorial;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << factorial(num);
+}
