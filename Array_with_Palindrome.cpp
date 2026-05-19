83c177b41efb059c80a270d24176948775db8d29
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-19 23:17:34 +0530

    Array with palindrome

--- Array_with_Palindrome.cpp
+++ Array_with_Palindrome.cpp
@@ -8,48 +8,47 @@ Summary - Return true if all array elements are palindrome
 #include <vector>
 using namespace std;
 
-bool isPalinArray(vector<int> &arr) 
+bool isPalinArray(vector<int> &arr)
+{
+    vector<int> v1;
+    vector<vector<int>> v;
+
+    for (int i = 0; i < arr.size(); i++)
     {
-        // code here
-        vector<int> v1;
-        vector<vector<int>> v;
-        
-        for(int i = 0;i<arr.size();i++)
+        v1.clear();
+        do
         {
-            v1.clear();
-            do
-            {
-                v1.push_back(arr[i]%10);
-                arr[i]/=10;
-            }while(arr[i] != 0);
-            v.push_back(v1);
-        }
-        
-        for(int i=0;i<v.size();i++)
-        {
-            for(int j=0;j<v[i].size()/2;j++)
-            {
-                if(v[i][j] != v[i][v[i].size() - 1 - j])
-                    return false;
-            }
-        }
-        
-        return true;
+            v1.push_back(arr[i] % 10);
+            arr[i] /= 10;
+        } while (arr[i] != 0);
+        v.push_back(v1);
     }
 
-    int main()
+    for (int i = 0; i < v.size(); i++)
     {
-        
-        vector<int> v;
-
-        int size;
-        cin >> size;
-        int inp;
-
-        for(int i=0;i<size;i++)
+        for (int j = 0; j < v[i].size() / 2; j++)
         {
-            cin >> inp;
-            v.push_back(inp);
+            if (v[i][j] != v[i][v[i].size() - 1 - j])
+                return false;
         }
-        cout << isPalinArray(v);
     }
+
+    return true;
+}
+
+int main()
+{
+
+    vector<int> v;
+
+    int size;
+    cin >> size;
+    int inp;
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> inp;
+        v.push_back(inp);
+    }
+    cout << isPalinArray(v);
+}
