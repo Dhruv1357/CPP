de33ba954fa79640b48655ae6910dae56ef68864
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-27 22:51:41 +0530

    Count number of set bits

---
+++ Count_set_bits.cpp
@@ -0,0 +1,29 @@
+/*
+Problem - Count Set Bits
+Platform - GeeksForGeeks
+Summary - Counting the number of bits set in a number
+*/
+
+#include <iostream>
+using namespace std;
+
+    int setBits(int n) {
+
+        int count = 0;
+        
+        for(int i = 0;i<32;i++)
+        {
+            if(n & (1<<i))
+                count++;
+        }
+        
+        return count;
+    }
+
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << setBits(num);
+}
