c9519ea51e95e505015c9b898f82d7fb8dbe497f
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-27 22:54:36 +0530

    Bit Manipulation

---
+++ Bit_Manipulation.cpp
@@ -0,0 +1,24 @@
+/*
+Problem - Bit Manipulation
+Platform - GeeksForGeeks
+Summary - Doing some bit operations on bit
+*/
+
+#include <iostream>
+using namespace std;
+
+void bitManipulation(int num, int i)
+{
+
+    bool get = num & (1 << (i - 1));
+    int set = num | (1 << (i - 1)), clear = num & ~(1 << (i - 1));
+
+    cout << get << " " << set << " " << clear;
+}
+
+int main()
+{
+    int n,i;
+    cin >> n >> i;
+    bitManipulation(n,i);
+}
