53c8a2c7b0c51dae506782da1945959be76a7fc7
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-30 23:46:43 +0530

    Count Unset Bits

---
+++ Count_Unset_bits.cpp
@@ -0,0 +1,30 @@
+/*
+Problem - Count unset bits in a given range
+Platform - GeeksForGeeks
+Summary - Counting the number of set bits in a given range
+*/
+
+#include <iostream>
+using namespace std;
+
+int countUnsetBits(int n, int l, int r)
+{
+    int count = 0;
+
+    for (int i = l - 1; i < r; i++)
+    {
+        if (!(n & (1 << i)))
+            count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+    int num, left, right;
+
+    cin >> num >> left >> right;
+
+    cout << countUnsetBits(num, left, right);
+}
