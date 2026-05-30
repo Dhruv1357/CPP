a630e6a05891986a9da79bdcbb463e0f99f12fc2
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-05-30 23:49:15 +0530

    Set all Bits in a given range

---
+++ Set_all_Bits.cpp
@@ -0,0 +1,27 @@
+/*
+Problem - Set all the bits in a given range
+Platform - GeeksForGeeks
+*/
+
+#include <iostream>
+using namespace std;
+
+int setAllRangeBits(int N, int L, int R) 
+{
+        
+        for(int i=L-1;i<R;i++)
+        {
+            N = N | (1 << i);
+        }
+        
+        return N;
+}
+
+int main()
+{
+    int num, left, right;
+
+    cin >> num >> left >> right;
+
+    cout << setAllRangeBits(num, left, right);
+}
