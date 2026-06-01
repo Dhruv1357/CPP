af52d16a3cdf549e3468d5c4b1351cf9d17183a7
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-06-01 23:08:12 +0530

    Longest 1s

---
+++ Longest_1.cpp
@@ -0,0 +1,37 @@
+/*
+Problem - Longest consecutive 1s
+Platform - GeeksForGeeks
+Summary - Find the length of the longest consecutive 1s in the binary representation of a number
+*/
+
+#include <iostream>
+using namespace std;
+
+int maxConsecutiveOnes(int n)
+{
+    // code here
+    int max = 0, length = 0;
+
+    for (int i = 0; i < 32; i++)
+    {
+        if (n & (1 << i))
+            length++;
+        else
+        {
+            if (length > max)
+                max = length;
+
+            length = 0;
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    cout << maxConsecutiveOnes(num);
+}
