e3debd2067fb4b44347c59f97871a102f834559c
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-06-02 22:53:57 +0530

    Is power of 2

---
+++ Power_of_two.cpp
@@ -0,0 +1,33 @@
+/*
+Problem - Power of 2
+Platform - GeeksForGeeks
+Summary - Is the given number a power of two
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isPowerofTwo(int n)
+{
+    // code here
+    int count = 0;
+
+    if (n == 0)
+        return false;
+    for (int i = 0; i < 32; i++)
+    {
+        if (n & (1 << i))
+            count++;
+        if (count == 2)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << isPowerofTwo(num);
+}
