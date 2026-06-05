54a2f626a0e47dab71da6daa87641ac7a48072c2
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-06-05 23:11:21 +0530

    Nega Bit

--- Nega_Bit.cpp
+++ Nega_Bit.cpp
@@ -7,22 +7,23 @@ Summary - Given two numbers,for every set bit in one number negate the bit in se
 #include <iostream>
 using namespace std;
 
-    int negaBit(int F, int S) {
+int negaBit(int F, int S)
+{
 
-        for(int i=0;i<32;i++)
-        {
-            if(F & (1<<i))
-            {
-                S = S ^ (1 << i);
-            }
-        }
-        
-        return S;
-    }
-
-    int main()
+    for (int i = 0; i < 32; i++)
     {
-        int num1,num2;
-        cin >> num1,num2;
-        cout << negaBit(num1,num2);
+        if (F & (1 << i))
+        {
+            S = S ^ (1 << i);
+        }
     }
+
+    return S;
+}
+
+int main()
+{
+    int num1, num2;
+    cin >> num1, num2;
+    cout << negaBit(num1, num2);
+}
