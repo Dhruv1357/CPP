9840ad8135b4d14fb79e752c8243a204baddb238
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-06-09 23:08:28 +0530

    Unique Number

---
+++ Unique_Number.cpp
@@ -0,0 +1,18 @@
+/*
+Problem - Unique Number 1
+Platform - GeeksForGeeks
+Summary - Given an array in which every element occurs exactly twice except for one element which occurs only once,find that element
+*/
+
+#include <iostream>
+using namespace std;
+
+int findUnique(vector<int> &arr)
+{
+    for (int i = 1; i < arr.size(); i++)
+    {
+        arr[0] = arr[0] ^ arr[i];
+    }
+
+    return arr[0];
+}
