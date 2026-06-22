639340ae27f1acf41828b8f79ce13d0cb44a60e3
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-06-22 23:01:21 +0530

    Finding LCM and GCD

--- LCM_GCD.cpp
+++ LCM_GCD.cpp
@@ -32,7 +32,7 @@ vector<int> lcmAndGcd(int a, int b) {
             }
         }
         
-        //gcd check
+
         for(int i=1;i<=small;i++)
         {
             if((a%i) ==0 && (b%i) == 0)
