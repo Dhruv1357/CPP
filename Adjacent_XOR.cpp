16fdaa28c6972c8c8ec3387d0ca3278e2e196279
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-07-08 23:03:08 +0530

    Adjacent XOR

---
+++ Adjacent_XOR.cpp
@@ -0,0 +1,25 @@
+/*
+Problem - Adjacent XOR
+Platform - GeeksForGeeks
+Summary - Replacing a number with the XOR of it with the adjacent number
+*/
+
+class Solution {
+  public:
+    void replaceElements(vector<int>& arr) 
+    {
+        int t1 = arr[0];
+        for(int i=0;i<arr.size();i++)
+        {
+            int t2 = arr[i];
+            if(i == 0)
+                arr[0] = arr[0] ^ arr[1];
+            else if(i<arr.size() - 1)
+                arr[i] = t1 ^ arr[i+1];
+            else
+                arr[arr.size() - 1] = t1 ^ arr[arr.size() - 1];
+                
+            t1 = t2;
+        }
+    }
+};
