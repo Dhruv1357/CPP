c4410ae39c480b16193065f5e69169b519a4e43c
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-07-13 22:38:07 +0530

    Checking if array is in AP

---
+++ Check_AP.cpp
@@ -0,0 +1,24 @@
+/*
+Problem - Check Arithmetic Progression
+Platform - GeeksForGeeks
+Summary - Check if the given array is in Arithmetic Progression or not
+*/
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    bool checkIsAP(vector<int> &arr) 
+    {
+        
+        sort(arr.begin(),arr.end());
+        int d = arr[1] - arr[0];
+        for(int i =1;i<arr.size();i++)
+        {
+            if((arr[i] - arr[i-1]) != d)
+                return false;
+        }
+        return true;
+    }
+};
