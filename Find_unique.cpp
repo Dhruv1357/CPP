c44e8876e9e1bc998545a482b26a9701a17fdabc
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-07-27 21:45:08 +0530

    Finding unique element

---
+++ Find_unique.cpp
@@ -0,0 +1,24 @@
+/*
+Problem - Find Unique Element
+Platform - GeeksForGeeks
+Summary - Every element is occuring in multiple of k, except for one element, find that element
+*/
+
+class Solution {
+  public:
+    int findUnique(int k, vector<int>& arr) 
+    {
+        unordered_map<int,int>um;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            um[arr[i]]++;
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            if(um[arr[i]] != k)
+                return arr[i];
+        }
+        
+    }
+};
