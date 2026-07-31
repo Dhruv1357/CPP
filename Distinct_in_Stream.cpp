d5e130f118f3365022b6462a458bd1b58a53fc4d
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-07-31 23:15:58 +0530

    Distinct in Stream

---
+++ Distinct_in_Stream.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> getDistinct(vector<int> &arr) {
+        // code here
+        unordered_map<int,int>um;
+        vector<int>v;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i] > 0)
+            {
+                um[arr[i]]++;
+            }
+            else if(arr[i] < 0 && um[arr[i]*(-1)] > 1)
+            {
+                um[arr[i]*(-1)]--;
+            }
+            else if(arr[i] < 0 && um[arr[i]*(-1)] <= 1)
+            {
+                um.erase(arr[i] * (-1));
+            }
+            v.push_back(um.size());
+        }
+        return v;
+    }
+};
