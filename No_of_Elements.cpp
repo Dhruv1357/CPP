c4415d6f4b039bbe6b67e5917c61636526ead311
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-04 22:56:04 +0530

    No. of Elements

---
+++ No_of_Elements.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Solution {
+  public:
+    int getCount(vector<int> &arr, int num1, int num2) 
+    {
+        // Your code goes here.
+        int count = 0,n1 = arr.size(),n2 = 0;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i] == num1 && n1 >= i)
+                n1 = i;
+            if(arr[i] == num2 && n2 <= i)
+                n2 = i;
+        }
+        if(n2 > n1)
+            count = n2 - n1 - 1;
+        else
+            count = 0;
+            
+        return count;
+    }
+};
