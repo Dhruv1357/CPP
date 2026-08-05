152246b90be4343f42499ecc84a4bbefda98299f
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-05 22:53:26 +0530

    Anagram Palindrome

---
+++ Anagram_Palindrome.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Solution {
+  public:
+    bool canFormPalindrome(string &s) 
+    {
+        // code here
+        int countOdd = 0;
+        unordered_map<char,int>um;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            um[s[i]]++;
+        }
+        for(auto ch : um)
+        {
+            if(ch.second%2 != 0)
+                countOdd++;
+        }
+        if(s.size()%2 == 0)
+        {
+            if(countOdd != 0)
+                return false;
+        }
+        else
+        {
+            if(countOdd != 1)
+                return false;
+        }
+        return true;
+        
+    }
+};
