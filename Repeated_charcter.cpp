b316d91edb008c161a3f61de21e6de7181af02e3
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-07 22:25:26 +0530

    Repeated Character

---
+++ Repeated_charcter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    char firstRep(string s) 
+    {
+        // code here.
+        unordered_map<char,int>um;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            um[s[i]]++;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            if(um[s[i]] > 1)
+                return s[i];
+        }
+        return '#';
+    }
+};
