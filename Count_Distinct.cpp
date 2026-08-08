6e3214cf97613415b8d360ffa451efa9ddde9ebb
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-08 22:52:31 +0530

    Counting Distinct Elements

---
+++ Count_Distinct.cpp
@@ -0,0 +1,23 @@
+// User function Template for C++
+int distinct(vector<int>& arr) 
+{
+    // Your code here
+    // unordered_set<int>us;
+    
+    // for(int i=0;i<arr.size();i++)
+    // {
+    //     us.insert(arr[i]);
+    // }
+    
+    // return us.size();
+    
+    sort(arr.begin(),arr.end());
+    int count = 1;
+    
+    for(int i=1;i<arr.size();i++)
+    {
+        if(arr[i] != arr[i-1])
+            count++;
+    }
+    return count;
+}
