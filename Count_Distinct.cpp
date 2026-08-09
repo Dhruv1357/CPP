e8d819f19d13fea9ed26ccc20f0e8e87a72695a0
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-09 23:00:16 +0530

    Counting Distinct Elements

--- Count_Distinct.cpp
+++ Count_Distinct.cpp
@@ -3,15 +3,7 @@ using namespace std;
 
 int distinct(vector<int>& arr) 
 {
-    // Your code here
-    // unordered_set<int>us;
     
-    // for(int i=0;i<arr.size();i++)
-    // {
-    //     us.insert(arr[i]);
-    // }
-    
-    // return us.size();
     
     sort(arr.begin(),arr.end());
     int count = 1;
