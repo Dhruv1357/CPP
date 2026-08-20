40e9ab08828ab09e3294fe8ff697e2fb60f332d9
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-20 23:01:49 +0530

    Divide the number

--- Divide_the_number.cpp
+++ Divide_the_number.cpp
@@ -14,7 +14,7 @@ int countWays(int N)
     
     for(int i=1;i<N;i++)
     {
-        
+
         if((i+i+i+i) > N)
         break;
         for(int j=i;j<N;j++)
@@ -35,6 +35,7 @@ int countWays(int N)
             }
         }
     }
+    
     return count;
 }
 int main()
