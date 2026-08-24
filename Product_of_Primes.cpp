d72c738b8faa973cbd52c4519813aed47bf5d8ec
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-24 23:00:47 +0530

    Product of Primes

---
+++ Product_of_Primes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int primeProduct(int L, int R) 
+    {
+        // code here
+        int flag,m = 1000000007,res;
+        long long product = 1;
+        for(int i=L;i<=R;i++)
+        {
+            flag = 1;
+            for(int j=2;j*j<=i;j++)
+            {
+                if(i%j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag)
+                product = ((product%m)*(i%m))%m;
+        }
+        res = product%m;
+        return res;
+    }
+};
