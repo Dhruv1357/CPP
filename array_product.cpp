17ac6dcb9d44c1c6310593e724053cc4d91d3701
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-29 23:32:45 +0530

    Product of array

---
+++ array_product.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+
+    long long int product(vector<int> &arr) 
+    {
+        // code here
+        long long product =1;
+        int m = 1000000007;
+        for(int i=0;i<arr.size();i++)
+        {
+            product = ((product%m) * (arr[i]%m))%m;
+        }
+        return product;
+    }
+};
