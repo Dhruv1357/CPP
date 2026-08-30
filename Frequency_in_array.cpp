e7ec99b8c1ac76b26d2cc38de0c9e03fab87dc2c
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-08-30 23:35:21 +0530

    Frequency of elements in the given array

--- Frequency_in_array.cpp
+++ Frequency_in_array.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    // Function to count the frequency of all elements from 1 to N in the array.
+
     vector<int> frequencyCount(vector<int>& arr) 
     {
         // code here
