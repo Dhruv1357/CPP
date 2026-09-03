49cdb8efc7138157ad4f691a349dc2b6e3b7b5a1
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-03 22:26:24 +0530

    Determining the type of array

---
+++ Type_of_Array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Solution 
+{
+  public:
+    int typeOfArr(vector<int>& arr) 
+    {
+        // code here.
+        int aCount = 0,dCount = 0;
+        
+        for(int i = 1;i<arr.size();i++)
+        {
+            if(arr[i] > arr[i-1])
+                aCount++;
+            else if(arr[i] < arr[i-1])
+                dCount++;
+        }
+        if(aCount == (arr.size() - 1))
+            return 1;
+        else if(dCount == (arr.size() - 1))
+            return 2;
+        else if(aCount < dCount)
+            return 3;
+        else if(aCount > dCount)
+            return 4;
+    }
+};
