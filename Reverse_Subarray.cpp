145ea95477e65332de75572f74a62ca46167be91
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-04 23:01:21 +0530

    Reversing the subarray

---
+++ Reverse_Subarray.cpp
@@ -0,0 +1,18 @@
+// User function template for C++
+class Solution {
+  public:
+    vector<int> reverseSubArray(vector<int> &arr, int l, int r) 
+    {
+        // code here
+        int temp,x = 1;
+        
+        for(int i=l;i<=(r+l)/2;i++)
+        {
+            temp = arr[i-1];
+            arr[i-1] = arr[r-x];
+            arr[r-x] = temp;
+            x++;
+        }
+        return arr;
+    }
+};
