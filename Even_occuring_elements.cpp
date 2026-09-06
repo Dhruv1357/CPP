72d68e1817c76b8ece0711f447d0078558384bc3
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-06 22:56:02 +0530

    Even occuring elements

---
+++ Even_occuring_elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> findEvenOccurrences(vector<int>& arr) 
+    {
+        // code here
+        unordered_map<int,int>um;
+        vector<int>v;
+        
+        for(int i =0;i<arr.size();i++)
+        {
+            um[arr[i]]++;
+        }
+        for(int i = 0;i<arr.size();i++)
+        {
+            if((um.find(arr[i]) != um.end()) && (um[arr[i]]%2 == 0))
+            {
+                v.push_back(arr[i]);
+                um.erase(arr[i]);
+            }
+        }
+        if(v.size() == 0)
+            v.push_back(-1);
+            
+        return v;
+    }
+};
