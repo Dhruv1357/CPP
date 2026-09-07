768cd5790ce0163e600b633b0024fa8adcf2918e
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-07 23:02:55 +0530

    Last Duplicate in a sorted array

---
+++ Last_Duplicate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<int> dupLastIndex(vector<int>& arr) 
+    {
+        // code here
+        unordered_map<int,int>um;
+        vector<int>v;
+        
+        for(int i=arr.size() - 1;i>0;i--)
+        {
+            if(arr[i] == arr[i-1])
+            {
+                v.push_back(i);
+                v.push_back(arr[i]);
+                break;
+            }
+        }
+        if(v.size() == 0)
+        {
+            v.push_back(-1);
+            v.push_back(-1);
+        }
+        return v;
+    }
+};
